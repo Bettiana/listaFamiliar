d99f248f8e3e534d5d058ecddfcddab14fc8c111
Bettiana <balle.bettiana@gmail.com>
2017-11-21 11:37:36 +0100

    listaOwner

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -6,4 +6,21 @@ module.exports = function(Listafamiliar) {
         context.args.data.owner = context.req.accessToken.userId;
         next();
     });
+    
+     Listafamiliar.afterRemote('create', function (context, Listafamiliar, next) {
+         // Buscamos la lista usuario de la base de datos
+         var Usuario = Listafamiliar.app.models.Usuario;
+         //busca los datos del usuario autenticado
+         var idUser=context.req.accessToken.userId;
+         //conocer el id de la lista que se ha guardado
+         var idLista=Listafamiliar.id;
+         //buscaos el usuario con esa id
+         Usuario.findById(idUser, function(err, usuario){
+             usuario.listaFamiliarId=idLista;
+             usuario.save(function(err){
+                 next();
+             });
+         });
+    });
+    
 };
